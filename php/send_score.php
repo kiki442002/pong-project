d635e8f43efbceb0bd458118616feffff0d78b06
PICOT Killian <picot.killian@outlook.fr>
2023-01-09 16:37:32 +0100

    ok

--- php/send_score.php
+++ php/send_score.php
@@ -15,8 +15,11 @@ $nickname = $_GET["nickname"];
 $score = $_GET["score"];
 
 $result = $mysqli->query("SELECT * FROM `player` WHERE `nickname` = '$nickname'");
+$row = $result->fetch_row();
 if ($result) {
-    $mysqli->query("UPDATE `player` SET `score`='$score' WHERE `nickname`='$nickname'");
+    if ($row[1] < $score) {
+        $mysqli->query("UPDATE `player` SET `score`='$score' WHERE `nickname`='$nickname'");
+    }
 } else {
 
     $mysqli->query("INSERT INTO `player` (`nickname`, `score`) VALUES ('$nickname', '$score')");
