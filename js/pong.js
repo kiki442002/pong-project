791a45ce91c85f1a1f9f8205085921d277ca3da0
PICOT Killian <picot.killian@outlook.fr>
2023-01-09 12:18:50 +0100

    ok

--- js/pong.js
+++ js/pong.js
@@ -252,6 +252,7 @@ playButton.addEventListener('click', () => {
 
 function send_score() {
     document.querySelector("form").remove();
+    var xhttp = new XMLHttpRequest();
     xhttp.onreadystatechange = function () {
         if (this.readyState == 4 && this.status == 200) {
             alert("Score Envoyer");
