551e3f645251162ec33cb7f5392957d170301ab8
PICOT Killian <picot.killian@outlook.fr>
2023-01-14 21:06:59 +0100

    ok

--- php/score.php
+++ php/score.php
@@ -13,39 +13,39 @@
 
 <body id="flex_page">
     <div>
-    <div id="head">
-        <a class="head_btn" href="pong.html"><button type="button">Jouer</button></a>
-        <a class="head_btn" id="home" href="../index.html"></a>
-        <a class="head_btn" href="score.php"><button type="button">Scores</button></a>
-    </div>
-    
-    <h1>Top 25 des meilleurs joueurs</h1>
+        <div id="head">
+            <a class="head_btn" href="pong.html"><button type="button">Jouer</button></a>
+            <a class="head_btn" id="home" href="../index.html"></a>
+            <a class="head_btn" href="score.php"><button type="button">Scores</button></a>
+        </div>
+
+        <h1>Top 25 des meilleurs joueurs</h1>
     </div>
     <table>
-    <tr>
-        <th class="col" scope="col">Rang</th>
-        <th class="col right" scope="col">Joueur</th>
-        <th class="col left" scope="col">Score</th>
-    </tr>
-    <?php
-    require("config.php");
-    $result = $mysqli->query("SELECT * FROM `player` ORDER BY score DESC");
-    $i = 0;
-    while ($row = mysqli_fetch_array($result, 2) and $i < 25) {
-        $i++;
-        echo "<tr>";
-        echo "<td class='rang'>#",$i,"</td>";
-        echo "<th scope='row' class='right'>",$row[0],"</th>";
-        echo "<td class = 'left'>",$row[1],"</td>";
-        echo "</tr>";
-    }
+        <tr>
+            <th class="col" scope="col">Rang</th>
+            <th class="col right" scope="col">Joueur</th>
+            <th class="col left" scope="col">Score</th>
+        </tr>
+        <?php
+        require("config.php");
+        $result = $mysqli->query("SELECT * FROM `player` ORDER BY score DESC");
+        $i = 0;
+        while ($row = mysqli_fetch_array($result, 2) and $i < 25) {
+            $i++;
+            echo "<tr>";
+            echo "<td class='rang'>#", $i, "</td>";
+            echo "<th scope='row' class='right'>", $row[0], "</th>";
+            echo "<td class = 'left'>", $row[1], "</td>";
+            echo "</tr>";
+        }
 
-    mysqli_free_result($result);
-    $mysqli->close(); ?>
-    
-</table>
+        mysqli_free_result($result);
+        $mysqli->close(); ?>
 
-  
+    </table>
+
+
 </body>
 
 
